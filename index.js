7dded17c9d16430b50887f889b5a58b92b158751
Kristina Stout <kristinalstout@gmail.com>
2023-06-13 09:21:42 -0400

    added arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = function (){
+    const equation = 2000/100;
+    return equation;
+}
+const square = parameter => parameter * parameter;
+const add = (parameter1,parameter2) => parameter1 + parameter2;
